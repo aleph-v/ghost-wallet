ecc802e9c25bd0605b9a7e8cc240f0a4f3ad4b01
pvienhage <pevienhage@gmail.com>
2018-11-21 13:02:13 -0500

    Contracts are near final.

--- contracts/identity.sol
+++ contracts/identity.sol
@@ -1,14 +1,40 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.4.24;
 
 import "./p256.sol";
 
 contract idenity is p256Lib{
 
     mapping(address => bool) isOwner;
-    bytes ownerKey;
+    uint256 public_qx; //The coordinates of the registered public key
+    uint256 public_qy;
+    bytes32[] validApplicationId;
+    bytes4 counter; //We use bytes4 since FIDO UF2 uses big endian for its counter
+    byte userPresance = 0x01;
 
-    constructor(bytes _ownerKey) public{
-      ownerKey = _ownerKey;
+    constructor(uint256 _public_qx, uint _public_qy, bytes32 applicationId, bytes4 _counter) public{
+        public_qx = _public_qx;
+        public_qy = _public_qy;
+        validApplicationId.push(applicationId);
+        counter = _counter;
+    }
+
+    //@Dev - The signature will be a NIST P-256 signature of the raw data signature from the U2F FIDO standard
+    function hardwareRootAccess(bytes4 _counter, bytes32 applicationParam,  uint r, uint s) external {
+        require(uint32(_counter) > uint32(counter));
+        require(isIn(validApplicationId, applicationParam));
+
+        bytes32 hash = keccak256(abi.encodePacked(msg.sender));
+        hash = sha256(abi.encodePacked(applicationParam, userPresance, _counter, hash));
+        //Use sha256 since UF2 is a non-ethereum system
+
+        require(verify(uint256(hash),r,s,public_qx, public_qy));
+        isOwner[msg.sender] = true;
+    }
+
+    //@Dev- Proxy forwarding for the idenity
+    function forward(address _to, bytes _calldata) public payable {
+      require(isOwner[msg.sender]);
+      require(_to.call.value(msg.value)(_calldata));
     }
 
     //@Dev - Add an account which can can access the idenity
@@ -17,18 +43,38 @@ contract idenity is p256Lib{
       isOwner[_add] = true;
     }
 
+    //@Dev - Add an applicationParam
+    function addAppParam(bytes32 applicationParam) external {
+        require(isOwner[msg.sender]);
+        validApplicationId.push(applicationParam);
+    }
+
+    //@Dev - Remove an applicationParam
+    function removeAppParam(bytes32 applicationParam) external {
+        require(isOwner[msg.sender]);
+        for(uint i =0 ; i < validApplicationId.length; i++){
+            if(validApplicationId[i] == applicationParam){
+                delete validApplicationId[i];
+            }
+        }
+    }
+
     //@Dev - Allows contracts to check if from is in the same idenity as owner
-    function verify(address _owner) external view{
+    function checkAddress(address _owner) external view{
         require(isOwner[_owner]);
     }
-    //@Dev - The signature will be a NIST P-256 signature of the raw data signature from the U2F FIDO standard
-    function hardwareRootAccess(uint32 _counter, bytes _sig) external view{
 
-       
+    function getChallange(address proposed) external pure returns(bytes32){
+        return(keccak256(abi.encodePacked(proposed)));
     }
 
-    function forward(address _to, bytes _calldata) public payable {
-      require(isOwner[msg.sender]);
-      require(_to.call.value(msg.value)(_calldata));
+    //@Dev - Unsorted array search
+    function isIn(bytes32[] memory array, bytes32 question) internal pure returns(bool){
+        for(uint i =0 ; i < array.length; i++){
+            if(array[i] == question){
+                return(true);
+            }
+        }
+        return(false);
     }
 }
--- contracts/p256.sol
+++ contracts/p256.sol
@@ -1,81 +1,230 @@
-pragma solidity 0.4.24;
+/*
+MIT License
 
-contract p256Lib{
-    uint constant n = 2^256 - 2^224 + 2^192 + 2^96 - 1;
-    uint constant a = 3;
-    uint constant b = 46214326585032579593829631435610129746736367449296220983687490401182983727876;
-    point G = point(0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296,0x4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5);
+Copyright (c) 2018 Lionello Lunesu
 
-    struct point{
-        uint x;
-        uint y;
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+pragma solidity ^0.4.24;
+
+
+contract ModExp {
+
+    // address constant MODEXP_BUILTIN = 0x0000000000000000000000000000000000000005;
+
+    function modexp(uint256 b, uint256 e, uint256 m) internal view returns(uint256 result) {
+        assembly {
+            let freemem := mload(0x40)
+            mstore(freemem, 0x20)
+            mstore(add(freemem,0x20), 0x20)
+            mstore(add(freemem,0x40), 0x20)
+            mstore(add(freemem,0x60), b)
+            mstore(add(freemem,0x80), e)
+            mstore(add(freemem,0xA0), m)
+            let _ := call(39240, 0x0000000000000000000000000000000000000005, 0, freemem, 0xC0, freemem, 0x20)
+            result := mload(freemem)
+        }
     }
 
-    function sigVerify(bytes32 hash, uint r, uint s, uint publicKey_x, uint publicKey_y) public returns(bool){
-        require(r < n && s < n);
+}
 
-        point memory publicKey;
-        publicKey.x = publicKey_x;
-        publicKey_y = publicKey_y;
 
-        uint s_inv = invmod(s, n);
-        uint u_1 = uint(hash) * s_inv;
+// Library for secp256r1, forked from https://github.com/tls-n/tlsnutils/blob/master/contracts/ECMath.sol
+contract p256Lib is ModExp {
 
-        if(u_1 < uint(hash)){
-            u_1 += 0-n; //Wrap around mod n
-        }else{
-            u_1 %= n;
+    //curve parameters secp256r1
+    uint256 constant A  = 115792089210356248762697446949407573530086143415290314195533631308867097853948;
+    uint256 constant B  = 41058363725152142129326129780047268409114441015993725554835256314039467401291;
+    uint256 constant GX = 48439561293906451759052585252797914202762949526041747995844080717082404635286;
+    uint256 constant GY = 36134250956749795798585127919587881956611106672985015071877198253568414405109;
+    uint256 constant P  = 115792089210356248762697446949407573530086143415290314195533631308867097853951;
+    uint256 constant N  = 115792089210356248762697446949407573529996955224135760342422259061068512044369;
+    uint256 constant H  = 1;
+
+
+    function verify(uint256 e, uint256 r, uint256 s, uint256 qx, uint256 qy) public returns(bool) {
+
+        uint256 w = invmod(s, N);
+        uint256 u1 = mulmod(e, w, N);
+        uint256 u2 = mulmod(r, w, N);
+
+        uint256[3] memory comb = calcPointShamir(u1, u2, qx, qy);
+
+        uint256 zInv2 = modexp(comb[2], P - 3, P);
+        uint256 x = mulmod(comb[0], zInv2, P); // JtoA(comb)[0];
+        return r == x;
+    }
+
+
+    function recover(uint256 e, uint8 v, uint256 r, uint256 s) public view returns(uint256[2]) {
+
+        uint256 eInv = N - e;
+        uint256 rInv = invmod(r, N);
+        uint256 srInv = mulmod(rInv, s, N);
+        uint256 eInvrInv = mulmod(rInv, eInv, N);
+
+        uint256 ry = decompressPoint(r, v);
+        uint256[3] memory q = calcPointShamir(eInvrInv, srInv, r, ry);
+
+        return JtoA(q);
+    }
+    //We lay out memory starting at zero 32 bytes public key X 32 bytes public
+    function calcPointShamir(uint256 u1, uint256 u2, uint256 qx, uint256 qy) private pure returns(uint256[3] R) {
+        uint256[3] memory G = [GX, GY, 1];
+        uint256[3] memory Q = [qx, qy, 1];
+        uint256[3] memory Z = ecadd(Q, G);
+
+        uint256 mask = 2**255;
+
+        // Skip leading zero bits
+        uint256 or = u1 | u2;
+        while (or & mask == 0) {
+            mask = mask / 2;
         }
 
-        uint u_2 = s_inv * r;
-
-        if(u_2 < s_inv){
-            u_2 += 0-n; //Wrap around mod n
-        }else{
-            u_2 %= n;
+        // Initialize output
+        if (u1 & mask != 0) {
+            if (u2 & mask != 0) {
+                R = Z;
+            }
+            else {
+                R = G;
+            }
+        }
+        else {
+            R = Q;
         }
 
-        point memory check = pointAddition(scalarMult(u_1, G), scalarMult(u_2, publicKey));
+        while (true) {
 
-        return(check.x == r);
+            mask = mask / 2;
+            if (mask == 0) {
+                break;
+            }
+
+            R = ecdouble(R);
+
+            if (u1 & mask != 0) {
+                if (u2 & mask != 0) {
+                    R = ecadd(Z, R);
+                }
+                else {
+                    R = ecadd(G, R);
+                }
+            }
+            else {
+                if (u2 & mask != 0) {
+                    R = ecadd(Q, R);
+                }
+            }
+        }
     }
 
-    function scalarMult(uint scalar, point p) internal pure returns(point){
-        //TODO; use the double and add method
+
+    function getSqrY(uint256 x) private pure returns(uint256) {
+        //return y^2=x^3+Ax+B
+        return addmod(mulmod(x, mulmod(x, x, P), P), addmod(mulmod(A, x, P), B, P), P);
     }
 
-    function pointAddition(point p, point q) internal pure returns(point ret){
-        uint lamda = (q.y - p.y)/(q.x - p.x);
-        ret.x = lamda*lamda - p.x - q.x;
-        ret.y = lamda*(p.x - ret.x) - q.x;
+
+    //function checks if point (x, y) is on curve, x and y affine coordinate parameters
+    function isPoint(uint256 x, uint256 y) public pure returns(bool) {
+        //point fulfills y^2=x^3+Ax+B?
+        return mulmod(y, y, P) == getSqrY(x);
     }
 
-    function pointDouble(point p) internal pure returns(point ret){
-        uint lamda = (3*p.x*p.x - a);
-        ret.x = lamda*lamda - 2*p.x;
-        ret.y = lamda*(p.x - ret.x) - p.x;
+
+    function decompressPoint(uint256 x, uint8 yBit) private view returns(uint256) {
+        //return sqrt(x^3+Ax+B)
+        uint256 absy = modexp(getSqrY(x), 1+(P-3)/4, P);
+        return yBit == 0 ? absy : -absy;
     }
 
-    function invmod(uint x, uint p) internal pure returns (uint) {
-        if (x == 0 || x == p || p == 0){
-            revert();
+
+    // point addition for elliptic curve in jacobian coordinates
+    // formula from https://en.wikibooks.org/wiki/Cryptography/Prime_Curve/Jacobian_Coordinates
+    function ecadd(uint256[3] memory _p, uint256[3]  _q) private pure returns(uint256[3] R) {
+
+        // if (_q[0] == 0 && _q[1] == 0 && _q[2] == 0) {
+        // 	return _p;
+        // }
+
+        uint256 z2 = mulmod(_q[2], _q[2], P);
+        uint256 u1 = mulmod(_p[0], z2, P);
+        uint256 s1 = mulmod(_p[1], mulmod(z2, _q[2], P), P);
+        z2 = mulmod(_p[2], _p[2], P);
+        uint256 u2 = mulmod(_q[0], z2, P);
+        uint256 s2 = mulmod(_q[1], mulmod(z2, _p[2], P), P);
+
+        if (u1 == u2) {
+            if (s1 != s2) {
+                //return point at infinity
+                return [uint256(1), 1, 0];
+            }
+            else {
+                return ecdouble(_p);
+            }
         }
 
-        if (x > p) x = x % p;
-        int t1;
-        int t2 = 1;
-        uint r1 = p;
-        uint r2 = x;
-        uint q;
-        while (r2 != 0) {
-            q = r1 / r2;
-            (t1, t2, r1, r2) = (t2, t1 - int(q) * t2, r2, r1 - q * r2);
-        }
-
-        if (t1 < 0){
-            return (p - uint(-t1));
-
-        }
-        return uint(t1);
+        u2 = addmod(u2, P - u1, P);
+        z2 = mulmod(u2, u2, P);
+        uint256 t2 = mulmod(u1, z2, P);
+        z2 = mulmod(u2, z2, P);
+        s2 = addmod(s2, P - s1, P);
+        R[0] = addmod(addmod(mulmod(s2, s2, P), P - z2, P), P - mulmod(2, t2, P), P);
+        R[1] = addmod(mulmod(s2, addmod(t2, P - R[0], P), P), P - mulmod(s1, z2, P), P);
+        R[2] = mulmod(u2, mulmod(_p[2], _q[2], P), P);
     }
+
+
+    //point doubling for elliptic curve in jacobian coordinates
+    //formula from https://en.wikibooks.org/wiki/Cryptography/Prime_Curve/Jacobian_Coordinates
+    function ecdouble(uint256[3] _p) private pure returns(uint256[3] R) {
+
+        if (_p[1] == 0) {
+            //return point at infinity
+            return [uint256(1), 1, 0];
+        }
+
+        uint256 z2 = mulmod(_p[2], _p[2], P);
+        uint256 m = addmod(mulmod(A, mulmod(z2, z2, P), P), mulmod(3, mulmod(_p[0], _p[0], P), P), P);
+        uint256 y2 = mulmod(_p[1], _p[1], P);
+        uint256 s = mulmod(4, mulmod(_p[0], y2, P), P);
+
+        R[0] = addmod(mulmod(m, m, P), P - mulmod(s, 2, P), P);
+        R[2] = mulmod(2, mulmod(_p[1], _p[2], P), P);	// consider R might alias _p
+        R[1] = addmod(mulmod(m, addmod(s, P - R[0], P), P), P - mulmod(8, mulmod(y2, y2, P), P), P);
+    }
+
+
+    //jacobian to affine coordinates transformation
+    function JtoA(uint256[3] p) private view returns(uint256[2] Pnew) {
+        uint zInv = invmod(p[2], P);
+        uint zInv2 = mulmod(zInv, zInv, P);
+        Pnew[0] = mulmod(p[0], zInv2, P);
+        Pnew[1] = mulmod(p[1], mulmod(zInv, zInv2, P), P);
+    }
+
+
+    //computing inverse by using fermat's theorem
+    function invmod(uint256 _a, uint _p) public view returns(uint256 invA) {
+        invA = modexp(_a, _p - 2, _p);
+    }
+
 }
